b9369b10ef47b4fa5d0fe951940a4587d781a98a
Your Name <you@example.com>
2022-10-20 00:57:33 +0300

    2 commit

---
+++ src/__test__/app.test.js
@@ -0,0 +1,14 @@
+import arrSort from "../app";
+
+const arrHero = [
+    {name: 'мечник', health: 10},
+    {name: 'маг', health: 100},
+    {name: 'лучник', health: 80},
+];
+
+const sortarrHero = [
+    {name: 'мечник', health: 10},
+    {name: 'маг', health: 100},
+    {name: 'лучник', health: 80},
+  ];
+
